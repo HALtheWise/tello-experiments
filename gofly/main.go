bc4ead0757c69949e8abdad8ee1941b7606a9f80
Eric Miller <eric@legoaces.org>
2019-03-05 19:25:53 -0500

    cgo working

--- gofly/main.go
+++ gofly/main.go
@@ -1,23 +1,35 @@
 package main
 
+/*
+#include <stdio.h>
+#include <stdlib.h>
+//#include "ceres/ceres.h"
+
+int myprint(char* s) {
+	printf("%s\n", s);
+	return 1;
+}
+*/
+import "C"
+
 import (
 	"fmt"
+	"unsafe"
 
-	"github.com/HALtheWise/tello-experiments/gofly/ceres"
 	"github.com/SMerrony/tello"
 )
 
 var _ tello.Tello
-var _ ceres.StringVector
 
 func example() int {
-	// cs := C.CString("Hello from stdio\n")
-	// n := C.myprint(cs)
-	// C.free(unsafe.Pointer(cs))
+	cs := C.CString("Hello from stdio\n")
+	n := C.myprint(cs)
+	_ = n
+	C.free(unsafe.Pointer(cs))
 
 	return int(100)
 }
 
 func main() {
-	fmt.Println("Hello World", example())
+	fmt.Println("Hello World ", example())
 }
