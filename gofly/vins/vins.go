e0c5a3f7a926d6db3568b6a008190aa7f4f22c15
Eric Miller <eric@legoaces.org>
2019-03-07 00:23:25 -0500

    Simplified VINS imports

--- gofly/vins/vins.go
+++ gofly/vins/vins.go
@@ -2,10 +2,9 @@ package vins
 
 /*
 #cgo CXXFLAGS: -I/opt/ros/melodic/include
-#cgo CXXFLAGS: -I/home/eric/catkin_ws_tello/devel/include
 #cgo CXXFLAGS: -I../../third_party/eigen
 #cgo CXXFLAGS: -I../../third_party/vins-fusion/camera_models/include
-#cgo LDFLAGS: -lopencv_core -lopencv_highgui -lopencv_imgproc
+#cgo LDFLAGS: -lopencv_core
 
 #include <stdlib.h>
 #include "vins.h"
