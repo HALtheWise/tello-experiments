e52813e0481616cd1b2bf28093b43ea964f6d6bb
Eric Miller <eric@legoaces.org>
2019-03-07 00:21:50 -0500

    Working compile of VINS

--- gofly/vins/vins.go
+++ gofly/vins/vins.go
@@ -5,10 +5,9 @@ package vins
 #cgo CXXFLAGS: -I/home/eric/catkin_ws_tello/devel/include
 #cgo CXXFLAGS: -I../../third_party/eigen
 #cgo CXXFLAGS: -I../../third_party/vins-fusion/camera_models/include
+#cgo LDFLAGS: -lopencv_core -lopencv_highgui -lopencv_imgproc
 
 #include <stdlib.h>
 #include "vins.h"
 */
 import "C"
-
-const X = 0
