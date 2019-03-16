7a4ef8ea89ab1daded0147aca72bfac44993dbf3
Eric Miller <eric@legoaces.org>
2019-03-16 16:20:18 -0400

    Working Tello control

--- gofly/go.mod
+++ gofly/go.mod
@@ -2,4 +2,7 @@ module github.com/HALtheWise/tello-experiments/gofly
 
 go 1.12
 
-require github.com/SMerrony/tello v0.9.3
+require (
+	github.com/SMerrony/tello v0.9.3
+	github.com/davecgh/go-spew v1.1.1
+)
--- gofly/main.go
+++ gofly/main.go
@@ -1,19 +1,39 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"time"
+
+	"github.com/SMerrony/tello"
+	"github.com/davecgh/go-spew/spew"
 )
 
-// var _ tello.Tello
-
-func example() int {
-	// cs := C.CString("Hello from stdio\n")
-	// n := C.myprint(cs)
-	// C.free(unsafe.Pointer(cs))
-
-	return int(100)
-}
+var _ tello.Tello
 
 func main() {
-	fmt.Println("Hello World", example())
+	drone := new(tello.Tello)
+	err := drone.ControlConnectDefault()
+	defer drone.ControlDisconnect()
+	if err != nil {
+		log.Fatalf("Unable to connect to Tello: %v", err)
+	}
+
+	drone.TakeOff()
+	time.Sleep(2.0 * time.Second)
+
+	drone.GetSSID()
+	drone.GetVersion()
+	drone.GetMaxHeight()
+	time.Sleep(100 * time.Millisecond)
+
+	data := drone.GetFlightData()
+	spew.Dump(data)
+
+	time.Sleep(3.0 * time.Second)
+	log.Println("Flipping")
+	drone.ForwardLeftFlip()
+	time.Sleep(2.0 * time.Second)
+	log.Println("Hand landing")
+	drone.PalmLand()
+	time.Sleep(5 * time.Second)
 }
